9eba8c5e26586c538e3881f1cea0aa84f059e604
Shekh Mohsin <mohsin.shekh235@gmail.com>
2026-05-24 03:40:05 +0600

    updated

--- resources/views/frontend/pages/contact.blade.php
+++ resources/views/frontend/pages/contact.blade.php
@@ -80,57 +80,80 @@
                                 </div>
                             </div>
                             <div class="col-lg-7">
-                                <div class="contact-inner">
-                                    <div class="contact-form">
-                                        <div id="form-messages" class="error"></div>
-                                        <form class="tmp-dynamic-form" id="contact-form" method="POST" action="https://inversweb.com/product/html/reeni/mailer.php">
-                                            <div class="contact-form-wrapper row">
-                                                <div class="col-lg-6">
-                                                    <div class="form-group">
-                                                        <input class="input-field" name="name" id="contact-name" placeholder="Your Name" type="text" required>
-                                                    </div>
-                                                </div>
+                            <div class="contact-inner">
+                                <div class="contact-form">
+                                
+                                    @if(session('success'))
+                                        <div class="alert alert-success">
+                                            {{ session('success') }}
+                                        </div>
+                                    @endif
 
-                                                <div class="col-lg-6">
-                                                    <div class="form-group">
-                                                        <input class="input-field" id="contact-phone" placeholder="Phone Number" type="tel" required>
-                                                    </div>
-                                                </div>
+                                    @if(session('error'))
+                                        <div class="alert alert-danger">
+                                            {{ session('error') }}
+                                        </div>
+                                    @endif
 
-                                                <div class="col-lg-6">
-                                                    <div class="form-group">
-                                                        <input class="input-field" id="contact-email" name="email" placeholder="Your Email" type="email" required>
-                                                    </div>
-                                                </div>
+                                    @if($errors->any())
+                                        <div class="alert alert-danger">
+                                            <ul class="mb-0">
+                                                @foreach($errors->all() as $error)
+                                                    <li>{{ $error }}</li>
+                                                @endforeach
+                                            </ul>
+                                        </div>
+                                    @endif
 
-                                                <div class="col-lg-6">
-                                                    <div class="form-group">
-                                                        <input class="input-field" type="text" id="subject" name="subject" placeholder="Subject">
-                                                    </div>
-                                                </div>
-
-                                                <div class="col-lg-12">
-                                                    <div class="form-group">
-                                                        <textarea class="input-field" placeholder="Your Message" name="message" id="contact-message" required></textarea>
-                                                    </div>
-                                                </div>
-
-                                                <div class="col-lg-12">
-                                                    <div class="tmp-button-here">
-                                                        <button class="tmp-btn hover-icon-reverse radius-round w-100" name="submit" type="submit" id="submit">
-                                                            <span class="icon-reverse-wrapper">
-                                        <span class="btn-text">Appointment Now</span>
-                                                            <span class="btn-icon"><i class="fa-sharp fa-regular fa-arrow-right"></i></span>
-                                                            <span class="btn-icon"><i class="fa-sharp fa-regular fa-arrow-right"></i></span>
-                                                            </span>
-                                                        </button>
-                                                    </div>
+                                    <form method="POST" action="{{ route('contact.store') }}">
+                                        @csrf
+                                        <div class="contact-form-wrapper row">
+                                            <div class="col-lg-6">
+                                                <div class="form-group">
+                                                    <input class="input-field" name="name" placeholder="Your Name" type="text" value="{{ old('name') }}" required>
                                                 </div>
                                             </div>
-                                        </form>
-                                    </div>
+
+                                            <div class="col-lg-6">
+                                                <div class="form-group">
+                                                    <input class="input-field" name="phone" placeholder="Phone Number" type="tel" value="{{ old('phone') }}" required>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-6">
+                                                <div class="form-group">
+                                                    <input class="input-field" name="email" placeholder="Your Email" type="email" value="{{ old('email') }}" required>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-6">
+                                                <div class="form-group">
+                                                    <input class="input-field" type="text" name="subject" placeholder="Subject" value="{{ old('subject') }}">
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-12">
+                                                <div class="form-group">
+                                                    <textarea class="input-field" placeholder="Your Message" name="message" required> {{ old('message') }} </textarea>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-12">
+                                                <div class="tmp-button-here">
+                                                    <button class="tmp-btn hover-icon-reverse radius-round w-100" name="submit" type="submit">
+                                                        <span class="icon-reverse-wrapper">
+                                                            <span class="btn-text">Appointment Now</span>
+                                                            <span class="btn-icon"><i class="fa-sharp fa-regular fa-arrow-right"></i></span>
+                                                            <span class="btn-icon"><i class="fa-sharp fa-regular fa-arrow-right"></i></span>
+                                                        </span>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
+                        </div>
                         </div>
                     </div>
                 </div>
--- resources/views/frontend/pages/project-details.blade.php
+++ resources/views/frontend/pages/project-details.blade.php
@@ -87,52 +87,78 @@
                                     <div class="row g-5 align-items-center">
                                         <div class="col-lg-12">
                                             <div class="contact-inner">
-                                                <div class="contact-form">
-                                                    <form class="tmp-dynamic-form" id="contact-form" method="POST" action="https://inversweb.com/product/html/reeni/mail.php">
-                                                        <div class="contact-form-wrapper row">
-                                                            <div class="col-lg-6">
-                                                                <div class="form-group">
-                                                                    <input class="input-field" name="contact-name" id="contact-name" placeholder="Your Name" type="text" required="">
-                                                                </div>
-                                                            </div>
+                                <div class="contact-form">
+                                
+                                    @if(session('success'))
+                                        <div class="alert alert-success">
+                                            {{ session('success') }}
+                                        </div>
+                                    @endif
 
-                                                            <div class="col-lg-6">
-                                                                <div class="form-group">
-                                                                    <input class="input-field" name="contact-phone" id="contact-phone" placeholder="Phone Number" type="number" required="">
-                                                                </div>
-                                                            </div>
+                                    @if(session('error'))
+                                        <div class="alert alert-danger">
+                                            {{ session('error') }}
+                                        </div>
+                                    @endif
 
-                                                            <div class="col-lg-6">
-                                                                <div class="form-group">
-                                                                    <input class="input-field" id="contact-email" name="contact-email" placeholder="Your Email" type="text">
-                                                                </div>
-                                                            </div>
+                                    @if($errors->any())
+                                        <div class="alert alert-danger">
+                                            <ul class="mb-0">
+                                                @foreach($errors->all() as $error)
+                                                    <li>{{ $error }}</li>
+                                                @endforeach
+                                            </ul>
+                                        </div>
+                                    @endif
 
-                                                            <div class="col-lg-6">
-                                                                <div class="form-group">
-                                                                    <input class="input-field" type="text" id="subject" name="subject" placeholder="Subject">
-                                                                </div>
-                                                            </div>
-                                                            <div class="col-lg-12">
-                                                                <div class="form-group">
-                                                                    <textarea class="input-field" placeholder="Your Message" name="contact-message" id="contact-message"></textarea>
-                                                                </div>
-                                                            </div>
-                                                            <div class="col-lg-12">
-                                                                <div class="tmp-button-here">
-                                                                    <button class="tmp-btn hover-icon-reverse radius-round w-100" name="submit" type="submit" id="submit">
-                                                                        <span class="icon-reverse-wrapper">
-                                                    <span class="btn-text">Appointment Now</span>
-                                                                        <span class="btn-icon"><i class="fa-sharp fa-regular fa-arrow-right"></i></span>
-                                                                        <span class="btn-icon"><i class="fa-sharp fa-regular fa-arrow-right"></i></span>
-                                                                        </span>
-                                                                    </button>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </form>
+                                    <form method="POST" action="{{ route('contact.store') }}">
+                                        @csrf
+                                        <div class="contact-form-wrapper row">
+                                            <div class="col-lg-6">
+                                                <div class="form-group">
+                                                    <input class="input-field" name="name" placeholder="Your Name" type="text" value="{{ old('name') }}" required>
                                                 </div>
                                             </div>
+
+                                            <div class="col-lg-6">
+                                                <div class="form-group">
+                                                    <input class="input-field" name="phone" placeholder="Phone Number" type="tel" value="{{ old('phone') }}" required>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-6">
+                                                <div class="form-group">
+                                                    <input class="input-field" name="email" placeholder="Your Email" type="email" value="{{ old('email') }}" required>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-6">
+                                                <div class="form-group">
+                                                    <input class="input-field" type="text" name="subject" placeholder="Subject" value="{{ old('subject') }}">
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-12">
+                                                <div class="form-group">
+                                                    <textarea class="input-field" placeholder="Your Message" name="message" required> {{ old('message') }} </textarea>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-12">
+                                                <div class="tmp-button-here">
+                                                    <button class="tmp-btn hover-icon-reverse radius-round w-100" name="submit" type="submit">
+                                                        <span class="icon-reverse-wrapper">
+                                                            <span class="btn-text">Appointment Now</span>
+                                                            <span class="btn-icon"><i class="fa-sharp fa-regular fa-arrow-right"></i></span>
+                                                            <span class="btn-icon"><i class="fa-sharp fa-regular fa-arrow-right"></i></span>
+                                                        </span>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
                                         </div>
                                     </div>
                                 </div>
